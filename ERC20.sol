aeb6c64905b78443e4e514ac6e4a2611d4c078de
sachincoder1 <sachinhlo232@gmail.com>
2022-05-20 15:22:18 +0530

    comment

--- ERC20.sol
+++ ERC20.sol
@@ -10,7 +10,8 @@ interface ERC20Interface {
     function allowance (address tokenOwner, address spender) external view returns (uint remaining);
     function approve (address spender, uint tokens) external returns (bool success);
     function transferFrom(address from, address to, uint tokens) external returns (bool success);
-
+    
+    // Events
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 
