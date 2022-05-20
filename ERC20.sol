006d7ee9c29e8e45269d9a27209806242cd3a210
sachincoder1 <sachinhlo232@gmail.com>
2022-05-20 15:45:27 +0530

    initialization of a contract

--- ERC20.sol
+++ ERC20.sol
@@ -14,5 +14,21 @@ interface ERC20Interface {
     // Events
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
 
+abstract contract Block is ERC20Interface {
+    string public name = "Block"; // This will be the name of token
+    string public symbol = "BLK"; // Just like bitcoin have BTC, ethereum has ETH we are going to give it BLK
+    uint public decimal = 0;  // upto how much decimal places are token is divisible, In ERC it's upto 20 decimal number but for this i am taking 0 because i am not going to divide this ERC token.
+     
+    uint public override totalSupply; // here override is writtern because i am using the same function which is already available in interface. 
+    address public founder; // founder of this token.
+
+    mapping(address => uint) public balances;
+
+    constructor () {
+         totalSupply = 100000;
+         founder = msg.sender;
+         balances[founder]  = totalSupply;
+    }
 }
